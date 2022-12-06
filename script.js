bb495e4715538741df69d2251ceeef369430f16a
termedcashew <xmanlogan34@gmail.com>
2022-12-06 20:18:28 +0000

    Fina product

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-
+document.getElementById("dinoSubmit").addEventListener("click", function(event) {
+    event.preventDefault();
+    const url = "https://dinosaur-facts-api.shultzlab.com/dinosaurs/random"// + "?q=" + value;
+    fetch(url)
+    .then(function(response) {
+      return response.json();
+    }).then(function(json) {
+        console.log(json);
+        let results = "<div class = 'dinoName'>";
+          results += '<p>' + json.Name + "</p>";
+          results += "</div>";
+          results += "<h2>Description:</h2>"
+          results += "<div class = 'dinoContent'>";
+          results += '<p>' + json.Description + "</p>";
+          results += "</div>"
+          document.getElementById("dinoResults").innerHTML = results;
+    });
+});
